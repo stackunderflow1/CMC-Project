d9ad2b1477f1c4a34d738a195b9c0d556d7c681c
stackunderflow1 <48003141+stackunderflow1@users.noreply.github.com>
2019-03-27 23:42:31 -0500

    Update and rename UserInterface.java to StudentUserInteraction.java

---
+++ src/edu/csbsju/cs/Interface/StudentUserInteraction.java
@@ -0,0 +1,36 @@
+package edu.csbsju.cs.Interaction;
+import controller.*;
+import entity.*;
+
+import java.util.*;
+
+/**
+ *stackUnderflow1
+ *@author Anton Andrews, Ryan Graham, Nathan Drees, Noah Lefebvre, DeAndre Bethell
+ *@version March 2019
+ */
+
+public class StudentUserInteraction {
+
+
+  public University viewUniversity(){
+  }
+  public Page viewAllSavedUniversitys() { 
+  }
+  public String acceptanceProbability() { 
+  }
+  public Page viewRecommendedUniversitys() {
+  }
+  
+  public void saveUniversity() { 
+  }
+  
+  public void searchUniversity() { 
+  }
+  
+  public void removeSavedUniversity(){
+  }
+  
+ 
+  
+}
