bf66e09f9b2c5bdfbe5ae510e34d5b452547ca2c
stackunderflow1 <48003141+stackunderflow1@users.noreply.github.com>
2019-03-31 16:32:23 -0500

    Update LogOnController.java

--- src/edu/csbsju/cs/Controllers/LogOnController.java
+++ src/edu/csbsju/cs/Controllers/LogOnController.java
@@ -1,17 +1,38 @@
+/*
+*packages
+*/
 package edu.csbsju.cs.Controllers;
+/*
+*imports
+*/
 import edu.csbsju.cs.Entity.*;
 import java.io.*;
 import java.util.*;
 
+/**
+ * This class is the LogOn controller that allows a user to access the database.
+ * @StackUnderflow
+ * @author DeAndre Bethell, Nathan Drees, Anton Andrews, Ryan Graham, Noah Lefebvre
+ * @version March 2019
+ */
 public class LogOnController 
 {
+/** private instance variables
+*/
 private Users user;
  private static ArrayList<Users> allU = new ArrayList<Users>();
 
+/*LogOn Constructor
+*/
 public LogOnController()
 {
 	this.user = user;
 }
+/** 
+*Enables the user to log into the database
+*@param String username, password
+*@returns the result of the logOn by stating whether a user or admin is loggin onto the database. If neither it returns a fail message or a deactivated user
+*/
 public static String logOn(String username, String password)
 {
 	allU = DataBaseController2.getAllUsers();
@@ -37,7 +58,11 @@ public static String logOn(String username, String password)
 	}
 	return result;
 }
-
+/** 
+* Allows the user to logout of the database
+*@param String username, password
+@returns successfully logged out or fail if the user fails to log out
+*/
 public  void logout(String username, String password)
 {
 	allU = DataBaseController2.getAllUsers();
