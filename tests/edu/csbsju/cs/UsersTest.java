760781461ee66750377512b6e066f20dd9dbc6f5
NLEFEBVRE001 <nlefebvre001@vplincsci068.ad.csbsju.edu>
2019-04-10 13:23:31 -0500

    userstest

--- tests/edu/csbsju/cs/UsersTest.java
+++ tests/edu/csbsju/cs/UsersTest.java
@@ -8,85 +8,97 @@ import edu.csbsju.cs.Entity.*;
 
 public class UsersTest {
 	Users u;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		u = new Users("dummy", "test", "testU", "password", 'u','Y');
-	}
-	
-	@Test
-	public void testUsersConstructor1() {
-		
+		u = new Users("dummy", "test", "dummytest", "password", 'u', 'Y');
 	}
 
-	@Test
-	public void testUsersConstructor2() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalArgumentException.class)
+	public void testUsersConstructorInvalidType() {
+		new Users("noah", "lefebvre", "nlefebvre", "pcm9", 'x', 'Y');
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testUsersConstructorInvalidStatus() {
+		new Users("noah", "lefebvre", "nlefebvre", "pcm9", 'u', 'j');
 	}
 
 	@Test
 	public void testGetFirstName() {
-		fail("Not yet implemented");
+		assertTrue("getFirstName() expected dummy", "dummy".equals(u.getFirstName()));
 	}
 
 	@Test
 	public void testGetLastName() {
-		fail("Not yet implemented");
+		assertTrue("getLastName() expected test", u.getLastName().equals("test"));
 	}
 
 	@Test
 	public void testGetUsername() {
-		fail("Not yet implemented");
+		assertTrue("getUsername() expected dummytest", u.getUsername().equals("dummytest"));
 	}
 
 	@Test
 	public void testGetPassword() {
-		fail("Not yet implemented");
+		assertTrue("getPassword() expected password", u.getPassword().equals("password"));
 	}
 
 	@Test
 	public void testGetType() {
-		fail("Not yet implemented");
+		assertTrue("getType() expected 'u'", u.getType() == 'u');
 	}
 
 	@Test
 	public void testGetStatus() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPrint() {
-		fail("Not yet implemented");
+		assertTrue("getStatus() expected 'Y'", u.getStatus() == 'Y');
 	}
 
 	@Test
 	public void testSetFirstName() {
-		fail("Not yet implemented");
+		u.setFirstName("dexter");
+		assertTrue("Sets first name to dexter", u.getFirstName().equals("dexter"));
 	}
 
 	@Test
 	public void testSetLastName() {
-		fail("Not yet implemented");
+		u.setLastName("morgan");
+		assertTrue("Sets last name to morgan", u.getLastName().equals("morgan"));
 	}
 
 	@Test
 	public void testSetUserName() {
-		fail("Not yet implemented");
+		u.setUserName("dmorg");
+		assertTrue("Sets username to dmorg", u.getUsername().equals("dmorg"));
 	}
 
 	@Test
 	public void testSetPassword() {
-		fail("Not yet implemented");
+		u.setPassword("icetruck");
+		assertTrue("Sets password to icetruck", u.getPassword().equals("icetruck"));
 	}
 
 	@Test
 	public void testSetStatus() {
-		fail("Not yet implemented");
+		u.setStatus('N');
+		assertTrue("Sets type to 'N'", u.getStatus() == 'N');
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetStatusInvalidParam() {
+		u.setStatus('d');
 	}
 
 	@Test
 	public void testSetType() {
-		fail("Not yet implemented");
+		u.setType('a');
+		assertTrue("Sets type to 'a'", u.getType() == 'a');
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetTypeInvalidParam() {
+		u.setType('x');
 	}
 
 }
